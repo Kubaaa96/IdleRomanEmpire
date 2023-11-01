bc7e6b9343b220799f2ef23244ee53d7c743f92f
Jakub Dobrowolski <jd.kuba96@gmail.com>
2023-11-01 19:24:51 +0100

    Checking with manual gcc compiler setting

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -77,6 +77,7 @@ function(set_project_warnings TARGET_NAME)
             -Wlogical-op   # warn about logical operations being used where bitwise were
             # probably wanted
             -Wuseless-cast # warn if you perform a cast to the same type
+            -std=c++23
             )
 
     if(MSVC)
